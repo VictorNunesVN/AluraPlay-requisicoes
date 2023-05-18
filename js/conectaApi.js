7c126c59b2ec368bcd363b8d30b07c3c130c5b94
VictorNunesVN <58942853+VictorNunesVN@users.noreply.github.com>
2023-05-18 10:45:27 -0300

    alteração
    Modificando os códigos e criando novos

---
+++ js/conectaApi.js
@@ -0,0 +1,10 @@
+async function listaVideos() {
+    const conexao = await fetch("http://127.0.0.1:3000/videos")
+    const conexaoConvertida = await conexao.json()
+    return conexaoConvertida
+
+}
+
+export const conectaApi = {
+    listaVideos 
+}
---
+++ js/mostrarVideos.js
@@ -0,0 +1,26 @@
+import { conectaApi } from "./conectaApi"
+
+const dataLista = document.queySelector("[dataLista]")
+
+function constroiCard() {
+    const video = document.createElement('li')
+    video.className('video_item')
+    video.innerHTML =
+        `
+<iframe width="100%" height="72%" src="https://www.youtube.com/embed/pA-EgOaF23I"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen>
+    </iframe>
+<div class="descricao-video">
+    <img src="./img/logo.png" alt="logo canal alura">
+    <h3>Qual é o melhor hardware para programação com Mario Souto</h3>
+    <p>236 mil visualizações</p>
+</div>
+    `
+    return video 
+}
+
+async function listaVideo(){
+    const lista = await conectaApi.listaVideos()
+}
